c63625815d0a1d40dfbffa6e7c3183cc74c0aabb
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2025-04-29 20:41:06 +0800

    :coffin: clean

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,9 @@
+<script setup lang="ts"></script>
+
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about-view">
+    <h1>AboutView</h1>
   </div>
 </template>
 
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
-</style>
+<style></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,14 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="home-view">
+    <h1>HomeView</h1>
+    <h2>You did it!</h2>
+    <div>
+      We are using Node.js <span id="node-version"></span>, Chromium
+      <span id="chrome-version"></span>, and Electron <span id="electron-version"></span>.
+    </div>
+  </div>
 </template>
+
+<style scoped></style>

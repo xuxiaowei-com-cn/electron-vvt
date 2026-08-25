51d824e17fc55c9ce3b13712fa85ee51c4a4f192
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2026-08-25 17:00:10 +0800

    :boom: 使用 npm create vue@latest 重新创建项目

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -6,7 +6,11 @@ import { counterStore } from '@/stores/counter'
   <section id="center">
     <h1>About</h1>
     <p>This is the about page.</p>
-    <p>Current count from Pinia store: <strong>{{ counterStore.count }}</strong></p>
-    <p>Double count: <strong>{{ counterStore.doubleCount }}</strong></p>
+    <p>
+      Current count from Pinia store: <strong>{{ counterStore.count }}</strong>
+    </p>
+    <p>
+      Double count: <strong>{{ counterStore.doubleCount }}</strong>
+    </p>
   </section>
 </template>
